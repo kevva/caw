1e1e69c76147553b4538e5b2b0f9e4f967a531f9
Kevin Martensson <kevinmartensson@gmail.com>
2015-07-30 14:50:52 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+'use strict';
+var url = require('url');
+var getProxy = require('get-proxy');
+var objectAssign = require('object-assign');
+var tunnelAgent = require('tunnel-agent');
+
+module.exports = function (proxy, opts) {
+	opts = opts || {};
+
+	if (typeof proxy !== 'string') {
+		opts = proxy;
+		proxy = getProxy();
+	}
+
+	if (!proxy) {
+		return;
+	}
+
+	proxy = url.parse(proxy);
+
+	var uriProtocol = opts.protocol === 'https' ? 'https' : 'http';
+	var proxyProtocol = proxy.protocol === 'https' ? 'Https' : 'Http';
+	var port = proxy.port || (proxyProtocol === 'https' ? 443 : 80);
+	var method = [uriProtocol, proxyProtocol].join('Over');
+
+	delete opts.protocol;
+
+	return tunnelAgent[method](objectAssign({
+		proxy: {
+			host: proxy.hostname,
+			port: port
+		}
+	}, opts));
+};
---
+++ readme.md
@@ -0,0 +1,49 @@
+# caw [![Build Status](https://travis-ci.org/kevva/caw.svg?branch=master)](https://travis-ci.org/kevva/caw)
+
+> Construct HTTP/HTTPS agents for tunneling proxies
+
+
+## Install
+
+```
+$ npm install --save caw
+```
+
+
+## Usage
+
+```js
+var caw = require('caw');
+var got = require('got');
+
+got('todomvc.com', {
+	agent: caw()
+}, function () {});
+```
+
+
+## API
+
+### caw(proxy, options)
+
+#### proxy
+
+Type: `string`
+
+Proxy URL.
+
+#### options
+
+Type: `object`
+
+##### protocol
+
+Type: 'string'  
+Default: 'http'
+
+Endpoint protocol.
+
+
+## License
+
+MIT © [Kevin Mårtensson](http://github.com/kevva)
